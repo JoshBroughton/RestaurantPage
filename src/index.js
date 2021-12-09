e8c44cacb315724cfb370969d1c76a3805e9cd6a
Joshua Broughton <josh.broughton47@gmail.com>
2021-12-08 20:57:49 -0700

    homepage module done, working on styling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World?");
+import homepage from './homepage';
+import anImage from './sushHomepage.jpeg';
+import './style.css';
+
+let myImage = new Image();
+myImage.src = anImage;
+
+content = document.getElementById("content");
+content.appendChild(homepage(myImage));
