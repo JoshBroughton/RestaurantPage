53bc80cf58072dc7f46dc4c7eba8b430ea0c9c7b
Joshua Broughton <josh.broughton47@gmail.com>
2022-02-15 19:12:32 -0700

    removing maps functionality for now

--- src/contact.js
+++ src/contact.js
@@ -4,14 +4,14 @@ let contact = () => {
     message.innerHTML = "Come join us in the in arctic cold heart of Calgary, Alberta!";
     message.id = "contactText";
 
-    let map = document.createElement("iframe");
-    map.id = "map";
-    map.width = "750";
-    map.height = "500";
-    map.frameBorder = "0";
-    map.style = "border:0";
-    map.src = "https://www.google.com/maps/embed/v1/place?key=" + API_KEY +
-    "&q=Calgary+Tower,Calgary+Canada";
+    // let map = document.createElement("iframe");
+    // map.id = "map";
+    // map.width = "750";
+    // map.height = "500";
+    // map.frameBorder = "0";
+    // map.style = "border:0";
+    // map.src = "https://www.google.com/maps/embed/v1/place?key=" + API_KEY +
+    // "&q=Calgary+Tower,Calgary+Canada";
 
     let divArray = [];
     divArray[0] = message;
